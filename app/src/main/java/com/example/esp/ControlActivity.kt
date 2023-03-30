b4b8bbd1dbbb99666157ff3abeea36cf74995a08
Maksim Ivanov <imv@zelax.ru>
2023-03-30 17:20:05 +0300

    Bluetooth permission fixed

--- app/src/main/java/com/example/esp/ControlActivity.kt
+++ app/src/main/java/com/example/esp/ControlActivity.kt
@@ -2,29 +2,53 @@ package com.example.esp
 
 import android.bluetooth.BluetoothManager
 import android.content.Intent
-import android.os.Build.VERSION_CODES.P
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.esp.databinding.ActivityControlBinding
-import com.example.esp.databinding.ActivityMainBinding
-import java.util.UUID
 
 class ControlActivity : AppCompatActivity(), ReceiveThread.Listener {
     private lateinit var binding: ActivityControlBinding
     private lateinit var actListLauncher: ActivityResultLauncher<Intent>
     lateinit var btConnection: BtConnection
     private var listItem: ListItem? = null
+    private val TAG = "PermissionDemo"
+    private val RECORD_REQUEST_CODE = 101
+    private val PERMISSIONS_STORAGE = arrayOf<String>(
+        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+        android.Manifest.permission.BLUETOOTH,
+        android.Manifest.permission.BLUETOOTH_PRIVILEGED
+    )
+    private val PERMISSIONS_LOCATION = arrayOf<String>(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+        android.Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+        android.Manifest.permission.BLUETOOTH_PRIVILEGED
+    )
+    /*companion object {
+        var PERMISSIONS = arrayOf(
+            android.Manifest.permission.BLUETOOTH,
+            android.Manifest.permission.READ_PHONE_STATE
+        )
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityControlBinding.inflate(layoutInflater)
         setContentView(binding.root)
         onBtListResult()
+        checkPermissions()
         init()
         binding.apply {
             bA.setOnClickListener{
@@ -40,6 +64,58 @@ class ControlActivity : AppCompatActivity(), ReceiveThread.Listener {
         val btManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         val btAdapter = btManager.adapter
         btConnection = BtConnection(btAdapter, this)
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.BLUETOOTH)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to bluetooth denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.BLUETOOTH),
+            RECORD_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                             permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            RECORD_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        val permission1 =
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permission2 =
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH)
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS_STORAGE,
+                1
+            )
+        } else if (permission2 != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                PERMISSIONS_LOCATION,
+                1
+            )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
